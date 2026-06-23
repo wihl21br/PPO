b6b5e78ba2766c0c1cc1e45595c32458eb408af6
William Inkari Huaman Lazo <wiinhula.2023@gmail.com>
2026-06-23 15:26:27 -0300

    Mudanca_23/6

--- codigo_PPO/main.js
+++ codigo_PPO/main.js
@@ -1,5 +1,6 @@
 const container = document.querySelector("#container-categoria");
 const inputCategoria = document.querySelector("#nova-cate");
+const dataItem = document.querySelector("#data-item")
 
 function fu() {
     const nomeDaCategoria = inputCategoria.value;
@@ -14,31 +15,55 @@ function fu() {
         <button id="excluir-categoria">excluir</button>
       </div>
       <div class="personalizar">
-        <input placeholder="Nome do Item" />
-        <input placeholder="Valor" />
-        <input placeholder="Data" />
-        <button id="excluir-item">excluir</button>
+      <input id="nome-item" placeholder="Digite o nome do novo item">   
+      <input id="data-item" placeholder="Digite o Nome do Item" type="date"/>
+      <input id="valor-item" placeholder="Digite o Valor do Item">
       </div>
+      <div id="container-item"></div>
       <div class="div-item">
-        <input id="adicao-item" type="button" value="adicionar um Item" />
+        <button id="adicionar-item">adicionar item</button>
         <h2 id="Valores">Valor total dos itens:</h2>
       </div>
     </div>
     </div>
     `;    
-    container.insertAdjacentHTML("beforeend", novoItem);
+    container.insertAdjacentHTML("afterbegin", novoItem);
     inputCategoria.value = "";
 }
 
-document.querySelector("#adicionar-categoria").addEventListener("click", fu);
-
-const botaoExcluir = document.querySelector("#excluir-categoria");
 
 container.addEventListener("click", function(event) {
-    if (event.target.id === "excluir-categoria") {
-        const divParaDestruir = event.target.closest(".tudo");
-        if (divParaDestruir) {
-            divParaDestruir.remove();
-        }
+
+  if (event.target.id === "excluir-categoria") {
+    event.target.closest(".tudo").remove();
+  }
+
+  if (event.target.id === "adicionar-item") {
+    const categoriaAtual = event.target.closest(".tudo");
+    const NomeItem = categoriaAtual.querySelector("#nome-item");
+    const dataItem = categoriaAtual.querySelector("#data-item")
+    const ValorItem = categoriaAtual.querySelector("#valor-item")
+    const containerItem = categoriaAtual.querySelector("#container-item");
+    const nomeItemValor = NomeItem.value.trim();
+    const dataItemValor = dataItem.value;
+    const mostrarData = dataItemValor.value;
+    const nomeItem = NomeItem.value
+
+    if (nomeItemValor && dataItemValor){
+    const novoi = `
+      <div id="item">
+        <h3>${nomeItem}</h3>
+        <p>${mostrarData}</p>
+        <button id="excluir-item" style="background-color: blue;color: white;border: none;">excluir</button>
+      </div>
+    `;
+    
+    containerItem.insertAdjacentHTML("afterbegin", novoi);
+    NomeItem.value = "";
+    }else{
+       alert("Por favor, preencha o nome e a data do item.");
     }
+    
+  }
 });
+document.querySelector("#adicionar-categoria").addEventListener("click", fu);
